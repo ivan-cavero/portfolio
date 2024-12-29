e16f40c57ade9280ef156c683ef462740eaf9b34
Ivan Cavero <dev@ivancavero.com>
2024-12-29 21:40:52 +0100

    feat: migrate to astro components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [tailwind()],
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "en",
